ef5928a6280a98299def5760fab60253bda188e9
Ali <alihsistu@gmail.com>
2019-07-24 14:55:26 +0800

    second merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,3 +35,4 @@ class ApplicationController < Sinatra::Base
   end
 end
 
+end
---
+++ public/lib/recode_racer.js
@@ -0,0 +1,63 @@
+// TODO: write your code here
+
+// addEventListener("keyup", function(event) {
+//   if (event.keyCode === 81) {
+//     var a = document.querySelector(".active1");
+//     a.classList.remove("active1");
+//     var b = a.nextElementSibling;
+//     if (b.classList.contains("finish")) {
+//       alert("red car has won.");
+//     }
+//     b.classList.add("active1");
+//   }
+// });
+
+// addEventListener("keyup", function(event) {
+//   if (event.keyCode === 80) {
+//     var a = document.querySelector(".active2");
+//     a.classList.remove("active2");
+//     var b = a.nextElementSibling;
+
+//     if (b.classList.contains("finish")) {
+//       alert("blue car has won.");
+//     }
+//     b.classList.add("active2");
+//   }
+// });
+addEventListener("keyup", function(event) {
+  if (event.keyCode === 81) {
+    var a = document.querySelector(".active1");
+    a.classList.remove("active1");
+    var b = a.nextElementSibling;
+    if (b.classList.contains("finish")) {
+      b.classList.add("active1");
+      alert("red car has won.");
+      window.location.reload(false);
+    }
+    b.classList.add("active1");
+  } else if (event.keyCode === 80) {
+    var a = document.querySelector(".active2");
+    a.classList.remove("active2");
+    var b = a.nextElementSibling;
+
+    if (b.classList.contains("finish")) {
+      b.classList.add("active2");
+      alert("blue car has won.");
+      window.location.reload(false);
+    }
+    b.classList.add("active2");
+  }
+});
+
+// addEventListener("keyup", function(event) {
+//   if (event.keyCode === 80) {
+//     var a = document.querySelector(".active2");
+//     a.classList.remove("active2");
+//     var b = a.nextElementSibling;
+
+//     if (b.classList.contains("finish")) {
+//       alert("blue car has won.");
+//     }
+//     b.classList.add("active2");
+//   }
+// });
